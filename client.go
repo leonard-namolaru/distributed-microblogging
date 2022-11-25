d244b5899c2cee951c293eb15b5707d7b46c80ab
lignier <hugo.lignier@gmail.com>
2022-11-25 23:14:15 +0100

    maj client

--- client.go
+++ client.go
@@ -10,14 +10,17 @@ import (
 	"crypto/tls"
 	"io"
 	"net"
+	"math/big"
+	"crypto/rand"
+	//"string"
 )
 
-type save struct {
+type id struct {
 	Name string 	`json:"name"`
 	Key string		`json:"key"`
 }
 
-type clientId struct {
+type peerId struct {
 	Username string 			`json:"username"`
 	Adresses []address			`json:"addresses"`
 	Key string					`json:"key"`
@@ -28,12 +31,20 @@ type address struct {
 	Port uint64 	`json:"port"`
 }
 
+var p, g, p1, zero, one, a, A, B, s big.Int // a is private key !
+
+var urlAddress, urlRegister, urlPublicKey, urlList url.URL
+
+var myName string
+var myId *id
+
+const NB_BITS = 768 // I don't know if it's the same NB_BITS from the server so I assume NB_BITS = 768 as in the tp
+const NB_BYTES = NB_BITS / 8
 
 func main(){
 
-	urlAddress := url.URL{Scheme: "https", Host: "jch.irif.fr:8443", Path: "/udp-address"}
-	//urlRegister := url.URL{Scheme: "https", Host: "jch.irif.fr:8443", Path: "/register"}
-	//urlPublicKey := url.URL{Scheme: "https", Host: "jch.irif.fr:8443", Path: "/server-key"}
+	initMyName("Hugo and Lenny")
+	initVar() // B and s are not initialized
 
 	me := CreateHttpClient()
 	body := getHttpResponse(me, urlAddress.String())
@@ -52,11 +63,6 @@ func main(){
 	connection.Close()
 }
 
-
-/*func SaveToServer() bool {
-
-}*/
-
 func CreateHttpClient() *http.Client {
 	t := http.DefaultTransport.(*http.Transport).Clone()
 	t.MaxIdleConns = 100
@@ -72,25 +78,13 @@ func CreateHttpClient() *http.Client {
 	return client
 }
 
-func createClientId(username string, ipv4 string, ipv6 string, port uint64, publicKey string) *clientId {
-	adress1Client := &address{
-		Ip: ipv4,
-		Port: port,
-	}
-	adress2Client := &address{
-		Ip: ipv6,
-		Port: port,
-	}
-	adressesClient := []address{
-		*adress1Client,
-		*adress2Client,
-	}
-	client := &clientId{
+func createPeerId(username string, addressesPeer []address, publicKey string) *peerId {
+	peer := &peerId{
 		Username: username,
-		Adresses: adressesClient,
+		Adresses: addressesPeer,
 		Key: publicKey,
 	}
-	return client
+	return peer
 } 
 
 
@@ -142,4 +136,36 @@ func postHttpResponse(client *http.Client, url string) []byte {
 	defer resp.Body.Close()
 
 	return body
+}
+
+func initVar(){
+
+	urlAddress = url.URL{Scheme: "https", Host: "jch.irif.fr:8443", Path: "/udp-address"}
+	urlRegister = url.URL{Scheme: "https", Host: "jch.irif.fr:8443", Path: "/register"}
+	urlPublicKey = url.URL{Scheme: "https", Host: "jch.irif.fr:8443", Path: "/server-key"}
+	urlList = url.URL{Scheme: "https", Host: "jch.irif.fr:8443", Path: "/peers"}
+
+	p.SetString("FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A63A36210000000000090563", 16)
+	g.SetInt64(2)
+	zero.SetInt64(0)
+	one.SetInt64(1)
+	p1.Sub(&p, &one)
+
+	buffer_of_bits := make([]byte, NB_BYTES)
+	_, errorMessage := rand.Read(buffer_of_bits)
+	if errorMessage != nil {
+		log.Fatal("rand.Read() function : ", errorMessage)
+	}
+
+	a.SetBytes(buffer_of_bits)
+	A.Exp(&g, &a, &p)
+
+	myId = &id{
+		Name: myName,
+		Key: A.String(),
+	}
+}
+
+func initMyName(name string){
+	myName = name
 }
